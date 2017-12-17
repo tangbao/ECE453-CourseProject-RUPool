3ce834e8a5cdd58ec92cca5abe8609100222d885
ZHU, ZICHEN <11926216+Frederick-Zhu@users.noreply.github.com>
2017-12-16 21:36:13 -0500

    将Adapter构造函数的第一个参数类型由ArrayList改为List

--- app/src/main/java/edu/rutgers/ece453/rupool/AdapterRecyclerViewMainFragment.java
+++ app/src/main/java/edu/rutgers/ece453/rupool/AdapterRecyclerViewMainFragment.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zhu_z on 2017/12/16.
@@ -14,10 +14,10 @@ import java.util.ArrayList;
 
 public class AdapterRecyclerViewMainFragment extends RecyclerView.Adapter<AdapterRecyclerViewMainFragment.ViewHolder> {
 
-    private ArrayList<PoolActivity> mData;
+    private List<PoolActivity> mData;
     private OnItemClickListener mOnItemClickListener;
 
-    AdapterRecyclerViewMainFragment(ArrayList<PoolActivity> data, OnItemClickListener onItemClickListener) {
+    AdapterRecyclerViewMainFragment(List<PoolActivity> data, OnItemClickListener onItemClickListener) {
         mData = data;
         mOnItemClickListener = onItemClickListener;
     }
